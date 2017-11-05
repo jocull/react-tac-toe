55f0ba3cc09d709ba7bf5542fa9ceef0509329e4
James O'Cull <jocull@delmarsd.com>
2017-11-05 15:41:50 -0500

    Working game! Neat!

--- src/App.js
+++ src/App.js
@@ -1,21 +1,131 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
+// class Square extends React.Component {
+//   render() {
+//     return (
+//       <button className="square" onClick={() => this.props.onClick()}>
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    }
+  }
+
+  handleClick(i) {
+    if (this.state.winner) {
+      return; // Do nothing - game is over
+    }
+
+    const squares = this.state.squares.slice(); // clone
+    if (squares[i]) {
+      return; // Already set
+    }
+
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+    const winner = this.calculateWinner(squares);
+
+    this.setState({
+      squares,
+      winner,
+      xIsNext: !this.state.xIsNext, // flip
+    });
+  }
+
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let line of lines) {
+      const [a, b, c] = line;
+      if (squares[a]
+        && squares[a] === squares[b]
+        && squares[a] === squares[c]) {
+
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  renderSquare(i) {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)} />
+    );
+  }
+
+  render() {
+    let status;
+    if (this.state.winner) {
+      status = 'Winner: ' + this.state.winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+    return (
+      <div>
+        <h1 style={this.state.winner? {display: 'block' } : {display: 'none'}}>GAME OVER</h1>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Game;
